711a35aa067c89223f52f9bd7fd2003040ea9a96
penshield.ceo@gmail.com <penshield.ceo@gmail.com>
2018-06-21 01:35:48 +0300

    Adding second assignment recent files

--- second_assignment/frontend/index.js
+++ second_assignment/frontend/index.js
@@ -1,3 +1,14 @@
 /**
  * Created by snouto on 20/06/18.
  */
+function Application(){
+    this.name=  "Second Assignment";
+}
+
+Application.prototype.init = function () {
+
+    //load the introduction into the container divv
+    var container = $("#main");
+
+
+};
---
+++ second_assignment/frontend/templates/intro.tpl
@@ -0,0 +1,6 @@
+<script type="mustache/x-tmpl" id="intro">
+     <div class="jumbotron" role="main">
+            <h1>Welcome Guest</h1>
+            <p>This site has been developed for Electricity Exchange.</p>
+        </div>
+</script>
